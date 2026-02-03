5447004664b39c36514ff0e35b202f303af70b87
Parv Chaudhary <mrabh@chaudhary>
2026-02-03 11:49:08 +0530

    add

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,7 +1,12 @@
 const fs= require('fs');
-//fs.writeFileSync("./ds-b.txt","hello abes college");
+//fs.writeFile("./ds-b.txt","hello abes college",()=>{});
 //fs.writeFileSync("./abes.txt","abes college");
-const result = fs.readFileSync("./abes.txt","utf-8");
-console.log(result);
-const result2 = fs.readFileSync("./ds-b.txt","utf-8");
-console.log(result2);
+// const result = fs.readFileSync("./abes.txt","utf-8");
+// console.log(result);
+fs.readFile("./ds-b.txt","utf-8",(err,data)=>{
+    if(err){
+        console.log("error",err);
+    }else{
+        console.log(data);
+    }
+ });
