127e4547a7af9538e021dc7ddf768ac417e2aae8
Parv Chaudhary <mrabh@chaudhary>
2026-02-03 12:08:07 +0530

    add

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,12 +1,56 @@
-const fs= require('fs');
-//fs.writeFile("./ds-b.txt","hello abes college",()=>{});
-//fs.writeFileSync("./abes.txt","abes college");
-// const result = fs.readFileSync("./abes.txt","utf-8");
-// console.log(result);
-fs.readFile("./ds-b.txt","utf-8",(err,data)=>{
+const fs=require('fs');
+
+fs.writeFile('./a1','A1',()=>{ });
+fs.writeFile('./b1','B1',()=>{ });
+fs.writeFile('./c1','C1',()=>{ });
+fs.writeFileSync('./d1','D1');
+fs.writeFileSync('./e1','E1');
+
+
+const result=fs.readFileSync('./abes.txt','utf-8');
+console.log(result);
+
+
+fs.readFile('./a1','utf-8',(err,result2)=>{
     if(err){
-        console.log("error",err);
-    }else{
-        console.log(data);
+        console.log('Error',err);
     }
- });
+    else{
+        console.log(result2);
+    }
+});
+
+fs.readFile('./b1','utf-8',(err,result3)=>{
+    if(err){
+        console.log('Error',err);
+    }
+    else{
+        console.log(result3);
+    }
+});
+
+fs.readFile('./c1','utf-8',(err,result4)=>{
+    if(err){
+        console.log('Error',err);       
+    }
+    else{
+        console.log(result4);
+    }
+});
+
+fs.readFileSync('./d1','utf-8');
+const result5=fs.readFileSync('./d1','utf-8');
+console.log(result5);
+
+fs.readFileSync('./e1','utf-8');
+const result6=fs.readFileSync('./e1','utf-8');
+console.log(result6);
+
+ fs.appendFile('./a1','\n pranav',(err)=>{
+    if(err){
+        console.log('Error',err);
+    }   
+    else{
+        console.log('Data appended successfully');
+    }
+});
